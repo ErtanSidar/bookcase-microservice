e8e11c2d1e04d51eabece01326e6770318f24b57
ErtanSidar <essoft3@gmail.com>
2024-10-31 20:28:54 +0300

    add book service codes

--- book-service/book-service/src/main/java/com/essoft/book_service/BookServiceApplication.java
+++ book-service/book-service/src/main/java/com/essoft/book_service/BookServiceApplication.java
@@ -1,13 +1,36 @@
 package com.essoft.book_service;
 
+import com.essoft.book_service.model.Book;
+import com.essoft.book_service.repository.BookRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
-public class BookServiceApplication {
+public class BookServiceApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookServiceApplication.class, args);
-	}
+    private final BookRepository bookRepository;
 
+    public BookServiceApplication(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Book book = new Book("Title", 2024, "Author", "Press", "ISBN");
+        Book book1 = new Book("Book 1", 2022, "Author 1","Press", "ISBN 1");
+        Book book2 = new Book("book2", 2023, "Author 2", "Press", "ISBN 2");
+        Book book3 = new Book("book3", 2024, "Author 3", "Press", "ISBN 3");
+
+        List<Book> books = bookRepository.saveAll(Arrays.asList(book, book1, book2, book3));
+
+        System.out.println(books);
+    }
 }
